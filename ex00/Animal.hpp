0f9a24063ff8c453833e1de14e3307862c07e540
jidrizi <idrizijuxhin@gmail.com>
2025-05-07 14:27:47 +0200

    created the base class which we will use throughout the entire project

---
+++ ex00/Animal.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Animal.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jidrizi <jidrizi@student.42heilbronn.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/07 14:22:09 by jidrizi           #+#    #+#             */
+/*   Updated: 2025/05/07 14:23:36 by jidrizi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ANIMAL_HPP
+# define ANIMAL_HPP
+
+#include <iostream>
+#include <string>
+
+class Animal
+{
+	protected:
+		std::string type;
+	public:
+		Animal();
+		Animal(const Animal &src);
+		virtual ~Animal();
+		Animal &operator=(const Animal &src);
+};
+
+#endif
